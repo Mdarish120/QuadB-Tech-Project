8a4d0f83a3735546923b3e58fc27df00e7e96919
Mohd Arish <arishmd768@gmail>
2024-04-01 21:46:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Input from "./components/Input";
+import ListItems from "./components/ListItems";
+
+function App() {
+  return (
+    <>
+      <Input />
+      <ListItems />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Input.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Box, TextField, Stack } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { addItem } from "../redux/Slice/toDoListSlice"; // Importing addItem action from Redux slice
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
+
+const Input = () => {
+  const [item, setItem] = useState(""); // State to store input value
+  const dispatch = useDispatch(); // Dispatch function for Redux actions
+
+  // Function to handle adding a new item
+  const handleAddItem = (e) => {
+    e.preventDefault(); // Preventing default form submission behavior
+    dispatch(addItem(item)); // Dispatching addItem action with the input value
+    console.log(item); // Logging the input value (optional)
+    setItem(""); // Clearing the input field after adding the item
+  };
+
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        marginTop="5rem"
+      >
+        <Stack spacing={2} direction="row">
+          {/* Text field for entering the item */}
+          <TextField
+            label="Enter Items"
+            variant="outlined"
+            onChange={(e) => setItem(e.target.value)} // Updating the item state as the user types
+            value={item} // Binding the value of the input field to the item state
+            size="medium"
+          />
+          {/* Button to add the item */}
+          <Fab
+            color="success"
+            aria-label="add"
+            size="medium"
+            onClick={handleAddItem} // Calling handleAddItem function when clicked
+          >
+            <AddIcon /> {/* Icon for the add button */}
+          </Fab>
+        </Stack>
+      </Box>
+    </>
+  );
+};
+
+export default Input;
